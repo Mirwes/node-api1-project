6b184d778267f5940cc423063199a90e9077026c
Mirwes <mirwes210@gmail.com>
2021-02-19 18:34:55 -0800

    updates the user with the specified id using data from the request body

--- api/server.js
+++ api/server.js
@@ -37,6 +37,32 @@ server.get('/api/users/:id', (req, res) => {
   });
 });
 
+//Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
+server.put('/api/users/:id', (req, res) => {
+    database.update(req.params.id, req.body)
+    .then(userID => {
+        if (!userID) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist." })
+        } else if (!req.body.name || !req.body.bio){
+            res.status(400).json({
+                errorMessage: "Please provide name and bio for the user." });
+        } else{
+            database.findById(userID)
+            .then(user => {
+                res.status(200).json(user);
+            }).catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    errorMessage: "The user info could not be retrieved." })
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "The user info could not be modified."})
+    });
+});
+
 
 
 
