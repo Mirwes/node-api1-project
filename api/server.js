0de938fc027e363272ad5c7cca298e9d103e5ae2
Mirwes <mirwes210@gmail.com>
2021-02-19 17:48:30 -0800

    get array of all user objects

--- api/server.js
+++ api/server.js
@@ -1,3 +1,31 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
+const cors = require('cors');
+const database  = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json());
+server.use(cors());
+
+// get array of all user objects
+
+server.get('/api/users', (req, res) => {
+    database.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+       console.log(err);
+       res.status(500).json({
+           errorMessage: "The users info couln't be retrieved."})
+    });
+});
+
+
+
+
+
+
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server');
+// const server = require('./api/server');
 
-const port = 5000;
+// const port = 5000;
 
 // START YOUR SERVER HERE
+const server = require("./api/server")
+
+server.listen(5000, () => {
+    console.log("Server started at localhost:5000.")
+})
